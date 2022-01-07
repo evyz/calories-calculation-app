50bf9b75a2c834062c954addf3e1ff57e5510083
evyz <endermainYT2502@gmail.com>
2022-01-07 16:09:27 +0300

    Загрузка шрифтов с рендером

--- App.js
+++ App.js
@@ -19,9 +19,9 @@ export default function App() {
       setIsReady(true)
     })
 
-    return (
-      <LoaderComponent />
-    )
+    // return (
+    //   <LoaderComponent />
+    // )
   }
 
   return (
