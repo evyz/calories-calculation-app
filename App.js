12b7e2dfefe50b26776023283f4ae1a8c8af7d31
evyz <endermainYT2502@gmail.com>
2022-01-07 16:03:08 +0300

    Загрузка шрифтов

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import AppRouter from './src/AppRouter';
 import AppProvider from './src/store';
+import * as Font from 'expo-font';
+import { useState } from 'react';
+
+async function loadAppAplication() {
+  await Font.loadAsync({
+    'montserrat-black': require('./vendors/Montserrat-Black.ttf'),
+  });
+}
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    loadAppAplication().then(data => {
+      setIsReady(true)
+    })
+  }
+
   return (
     <AppProvider>
       <AppRouter />
