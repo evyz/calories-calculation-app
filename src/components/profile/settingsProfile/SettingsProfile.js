e733c575e5fdf5a9baf59e62fb7de5a19a973952
evyz <endermainYT2502@gmail.com>
2022-01-08 18:23:53 +0300

    Проработана логика с иконками для профиля и редирект на ютуб

--- src/components/profile/settingsProfile/SettingsProfile.js
+++ src/components/profile/settingsProfile/SettingsProfile.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default SettingsProfileComponent = ({ navigation }) => {
     return (
         <View>
             <Text>SettingsProfileComponent</Text>
+            <Button title="Сбросить параметры" onPress={async () => {
+                await AsyncStorage.setItem('isWellcome', 'true')
+            }} />
         </View>
     )
 }
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
-    
+
 export default SettingsComponent = () => {
     return (
         <View>
             <Text>SettingsComponent</Text>
+
         </View>
     )
 }
