e3bf4a290faa5a8960dc34b9dd9ada123fd249e2
evyz <endermainYT2502@gmail.com>
2022-03-12 14:04:53 +0300

    added endpoint with refresh

--- src/http/user.js
+++ src/http/user.js
@@ -4,3 +4,8 @@ export const login = async (email, password) => {
   const { data } = await $host.post('api/user/login', { email, password })
   return data
 }
+
+export const refreshToken = async () => {
+  const { data } = await $authHost.get('api/user/refresh')
+  return data
+}
