d666b58c11c63404caa926cd4044453686d931b6
evyz <endermainYT2502@gmail.com>
2022-03-18 13:39:47 +0300

    added EyeIcon

---
+++ src/icons/visible/eye.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Svg, Path } from 'react-native-svg'
+
+const Eye = () => {
+  return (
+    <Svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Path d="M14.2268 5.25857C14.5654 5.70143 14.5654 6.29929 14.2268 6.74143C13.1604 8.13357 10.6018 11 7.6147 11C4.62756 11 2.06899 8.13357 1.00256 6.74143C0.837833 6.52938 0.748413 6.26851 0.748413 6C0.748413 5.73149 0.837833 5.47062 1.00256 5.25857C2.06899 3.86643 4.62756 1 7.6147 1C10.6018 1 13.1604 3.86643 14.2268 5.25857Z" stroke="#C4C4C4" stroke-linecap="round" stroke-linejoin="round" />
+      <Path d="M7.6147 8.14286C8.79817 8.14286 9.75756 7.18347 9.75756 6C9.75756 4.81653 8.79817 3.85714 7.6147 3.85714C6.43123 3.85714 5.47185 4.81653 5.47185 6C5.47185 7.18347 6.43123 8.14286 7.6147 8.14286Z" stroke="#C4C4C4" stroke-linecap="round" stroke-linejoin="round" />
+    </Svg>
+  )
+}
+
+export default Eye;
