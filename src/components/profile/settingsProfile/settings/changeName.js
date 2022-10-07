2c549e3c6380d3f17f22e86a957e080dd85b1ece
lowningonk <alesyabur2018@gmail.com>
2022-10-07 20:48:15 +0300

    смена имени с проверкой

--- src/components/profile/settingsProfile/settings/changeName.js
+++ src/components/profile/settingsProfile/settings/changeName.js
@@ -27,20 +27,94 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { setLastsAuth } from "../../../../storage/last.auth";
 import { changePassword } from "../../../../http/user";
 import ArrowLeft from "../../../Arrows/ArrowLeft";
+import { BOLD_FONT, MEDIUM_FONT } from "../../../../styles/fonts";
+
+let symbols = /[0-9a-zA-Z!@#$%^&*]{6,}/g;
 
 const ChangeName = observer(({ navigation }) => {
+  const [mail, setMail] = useState(false);
+  const [currentMail, setCurrentMail] = useState(false);
+  const [mailError, setMailError] = useState(false);
+  const [mailDirty, setMailDirty] = useState(false);
+
+  const blurHandler = (e) => {
+    setMail(e.nativeEvent.text);
+    if (e.nativeEvent?.text.length === 0) {
+      setMailError("Поле не может быть пустым");
+      setMailDirty(true);
+    } else {
+      setMailError("");
+      setMailDirty(false);
+    }
+  };
+
   return (
     <View style={[styles.main, { paddingTop: StatusBar.currentHeight }]}>
       <View>
         <TouchableOpacity
           onPress={() => navigation.navigate("settings")}
-          style={styles.backButton}
+          style={[styles.backButton, { marginTop: StatusBar.currentHeight }]}
         >
           <ArrowLeft />
         </TouchableOpacity>
-        <Text style={{ fontSize: 18, backgroundColor: "black" }}>
-          Смените пароль
+        <Text
+          style={{
+            fontSize: 24,
+            color: "black",
+            fontFamily: BOLD_FONT,
+            marginLeft: 20,
+            marginTop: 20,
+          }}
+        >
+          Сменить имя
         </Text>
+      </View>
+      <TextInput
+        onEndEditing={(e) => blurHandler(e)}
+        onChangeText={setCurrentMail}
+        value={mail}
+        placeholder="Введите новое имя"
+        autoCorrect={false}
+        autoCapitalize={"none"}
+        keyboardType="default"
+        style={styles.input}
+      />
+      {mailDirty ? (
+        <>
+          <Text
+            style={{
+              color: RED_COLOR,
+              fontSize: 14,
+              marginTop: 10,
+              marginLeft: 20,
+            }}
+          >
+            {mailError}
+          </Text>
+        </>
+      ) : (
+        <Text style={{ padding: 0, margin: 0 }}></Text>
+      )}
+      <View style={styles.login}>
+        <TouchableOpacity
+          style={{
+            width: "100%",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => navigation.navigate("main")}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              color: LIGHT_COLOR,
+              fontFamily: MEDIUM_FONT,
+            }}
+          >
+            Сменить имя
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -49,17 +123,48 @@ export default ChangeName;
 
 const styles = StyleSheet.create({
   main: {
-    flexDirection: "column",
     display: "flex",
     width: "100%",
     height: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    backgroundColor: LIGHT_COLOR,
   },
   backButton: {
     width: 100,
     display: "flex",
     flexDirection: "row",
-    marginLeft: 10,
+    marginLeft: 20,
+  },
+  login: {
+    display: "flex",
+    flexDirection: "row",
+    height: 50,
+    width: "90%",
+    marginTop: 45,
+    marginLeft: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 24,
+    backgroundColor: GREEN_COLOR,
+    borderColor: GREEN_COLOR,
+  },
+  input: {
+    width: "85%",
+    paddingLeft: 10,
+    borderStyle: "solid",
+    borderWidth: 3,
+    borderColor: 0,
+    borderBottomColor: GREY_COLOR,
+    fontSize: 12,
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 40,
+    marginLeft: 20,
+    fontSize: 14,
+    fontFamily: MEDIUM_FONT,
   },
 });
